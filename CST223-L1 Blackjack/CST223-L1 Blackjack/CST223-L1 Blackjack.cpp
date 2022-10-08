87041172915ce470089e933289b3976a50021f85
TaylorOIT <71860640+TaylorOIT@users.noreply.github.com>
2022-10-07 22:29:48 -0700

    Created a deck with a vector of tuples.

---
+++ CST223-L1 Blackjack/CST223-L1 Blackjack/CST223-L1 Blackjack.cpp
@@ -0,0 +1,33 @@
+// Taylor Boyd, CST223 lab 1
+// Blackjack
+
+#include <iostream>
+#include <vector>
+#include <tuple>
+
+using std::vector;
+using std::string;
+using std::tuple;
+# testststststs
+
+int main()
+{
+	string cardfaces[13] = { "Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "King", "Jack", "Queen" };
+	string cardsuits[4] = { "Diamonds", "Clubs", "Hearts", "Spades" };
+	int cardvalues[13] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10 };
+
+	vector< tuple <string, string, int> > Deck;
+	// includes the cardface, cardsuit, and value of the card in blackjack
+
+	for (int j = 0; j < 4; j++)
+	{
+		for (int i = 0; i < 13; i++)
+		{
+			Deck.push_back(std::make_tuple(cardfaces[i], cardsuits[j], cardvalues[i]));
+		}
+	}
+
+	std::cout << std::get<0>(Deck[13]) << " " << std::get<1>(Deck[13]) << " " << std::get<2>(Deck[13]);
+
+}
+
